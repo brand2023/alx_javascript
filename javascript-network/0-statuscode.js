0a56fe114a6568744b0ddb50dbb9387140c60e6f
brand2023 <brandonestephane@gmail.com>
2023-09-25 00:58:58 +0200

    Status code

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -1,4 +1,4 @@
 const request = require('request');
-request('https://intranet.alxswe.com') {
-  console.log('code:', response && response.statusCode);
-}
+request('https://intranet.alxswe.com', function (response){
+    console.log('code:', response && response.statusCode);
+})
