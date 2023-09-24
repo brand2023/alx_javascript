e7c90caa111765271e8493ac873803092ce14842
brand2023 <brandonestephane@gmail.com>
2023-09-24 18:15:38 +0200

    Square #1

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const Base = require('./5-square');
+class Square extends Base{
+    constructor (size){
+        super(size, size)
+    }
+
+    charPrint(c) {
+        if (c === undefined) {
+            c = 'X';
+          }
+        for (let i = 0; i < this.height; i++){
+            console.log(c.repeat(this.width))
+        }
+    }
+}
+
+
+
+module.exports = Square;
