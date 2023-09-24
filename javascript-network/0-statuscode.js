8175261b915f0d5ba66294233d9039e9d4532c35
brand2023 <brandonestephane@gmail.com>
2023-09-25 01:11:29 +0200

    Status code

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const request = require('request');
-request.get(process.argv[1], function (response){
+request.get(process.argv[1], {encoding: 'UTF-8'}, function (response){
     console.log('code:', response.statusCode);
 })
